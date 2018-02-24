c6194e05ca7a5f8747ddf43dd535c9ce36aa283d
vegalious <668es218pur@gmail.com>
2018-02-24 10:00:13 -0600

    Update sketch.js

--- sexscii/sketch.js
+++ sexscii/sketch.js
@@ -43,7 +43,7 @@ function draw()
   push();
   translate( x, y );
   rotate( sin( millis() / 100 ) / 10 );
-  textSize( sin( millis() / 100 ) * 10 + 70 );
+  textSize( sin( millis() / 100 ) * 10 + 100 );
   textAlign( CENTER );
   text( anim[currentFrame], 0, 0 );
   pop();
