cdcdfdba047c222b9c67a4f40fb3f0294e4a42cc
vegalious <668es218pur@gmail.com>
2018-02-25 08:24:37 -0600

    new sketch

--- sexscii/sketch.js
+++ sexscii/sketch.js
@@ -20,31 +20,43 @@ var deltaTime;
 var lastTime;
 var timer = 0;
 
+var panSound;
+
 function setup()
 {
+  //  prep drawing field
   createCanvas( windowWidth, windowHeight );
   frameRate( 1000 );
   noSmooth();
   textAlign( CENTER );
 
+  //  load assets
+  panSound = loadSound( 'sounds/pan.mp3' );
 
+  //  set state
   lastTime = millis();
 }
 
 function draw()
 {
   background( 255 );
-  stroke( 0 );
-  fill( 0 );
 
+  //  shift to center for drawing
+  push();
   var x = width / 2;
   var y = height / 2;
-
-  push();
   translate( x, y );
   rotate( sin( millis() / 100 ) / 10 );
+
+  //  set draw conditions
+  stroke( 0 );
+  fill( 0 );
   textSize( sin( millis() / 100 ) * 10 + 40 );
+
+  //  draw
   text( anim[currentFrame], 0, 0 );
+
+  //  shift back
   pop();
 
   //  deal with deltaTime
@@ -53,7 +65,7 @@ function draw()
   if( timer > frameDuration )
   {
     timer = 0
- 
+
     //  check for on first frame and going backwards
     if( (currentFrame === 0) && (animDirection === -1) )
     {
@@ -69,5 +81,12 @@ function draw()
     currentFrame = currentFrame + animDirection;
   }
 
+  //  play sound if its last frame
+  if( currentFrame == (anim.length - 1) )
+  {
+    panSound.play();
+  }
+
+  //  update state
   lastTime = millis();
 }
