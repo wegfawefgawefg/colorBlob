686a2b4fb4a3ebf2cf0c043934470cee80c32b27
vegalious <668es218pur@gmail.com>
2018-02-26 17:58:27 -0600

    sketch replacement

--- userDropFile/sketch.js
+++ userDropFile/sketch.js
@@ -1,12 +1,13 @@
 var canvas;
-var graphics;
 var userImage;
 var imageLoaded = false;
+
+var circleWidth = 50;
+var circleHeight = 50;
 
 function setup() {
 	// setup canvas
 	canvas = createCanvas( 800, 800 );
-	graphics = createGraphics( 100, 100 );
 	frameRate(1000);
 
 	//	color default look of canvas
@@ -24,8 +25,25 @@ function draw()
 {
 	if( imageLoaded === true )
 	{
-		image( graphics, 0, 0, 800, 800 );
-		modifyImage();
+		//image( userImage, 0, 0, 800, 800 );
+
+		//	pick a random x and y in the image
+		var randomX = random( userImage.width );
+		var randomY = random( userImage.height );
+
+		//	calculate equivalently scaled x and y in the Canvas
+		var x = map( randomX, 0, userImage.width, 0, 800 );
+		var y = map( randomY, 0, userImage.height, 0, 800 );
+
+		//	sample the image pixel color
+		userImage.loadPixels();
+		var color = userImage.get( floor(randomX), floor(randomY) );
+
+		//	draw circles in the canvas with sampled image pixel color
+		fill( red(color), green(color), blue(color), 50 );
+		noStroke();
+		ellipseMode( CENTER );
+		ellipse( x, y, circleWidth, circleHeight );
 	}
 }
 
@@ -33,33 +51,15 @@ function gotFile( file )
 {
 	canvas.removeClass('dashed');
 	userImage = loadImage( file.data );
-	
+
 	if(file.type === 'image')
 	{
 		imageLoaded = true;
+		background( 255, 255, 255 );
 	}
 }
 
-// function modifyImage()
-// {
-// 	graphics.image( userImage, 0, 0, 100, 100 );
-//
-// 	userImage.loadPixels();
-// 	var randomX = floor( random( 100 ) );
-// 	var randomY = floor( random( 100 ) );
-// 	console.log( randomX, randomY );
-// 	userImage.set( randomX, randomY, color( 0, 255, 0 ) );
-// 	userImage.updatePixels();
-// }
-
-function modifyImage()
+function clamp( number, min, max )
 {
-	graphics.image( userImage, 0, 0, 100, 100 );
-
-	userImage.loadPixels();
-	var randomX = floor( random( 100 ) );
-	var randomY = floor( random( 100 ) );
-	console.log( randomX, randomY );
-	userImage.set( randomX, randomY, color( 0, 255, 0 ) );
-	userImage.updatePixels();
+	return min( max, max( min, number ));
 }
