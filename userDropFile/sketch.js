7146a0898bf7290d7c7cdeceb2bcafcdcd00035c
vegalious <668es218pur@gmail.com>
2018-02-26 13:18:21 -0600

    testOfUserDropFile

---
+++ userDropFile/sketch.js
@@ -0,0 +1,45 @@
+var canvas;
+var userImage;
+var imageLoaded = false;
+
+function setup() {
+	// setup canvas
+	canvas = createCanvas( 800, 800 );
+	frameRate(1000);
+
+	//	color default look of canvas
+	canvas.background( 200, 200, 200 );
+	textSize( 24 );
+	textAlign( CENTER );
+	text( 'Drop An Image Into The Canvas!', width / 2, height / 2 );
+
+	canvas.id('mainCanvas');
+	canvas.addClass('dashed');
+	canvas.drop( gotFile );
+}
+
+function draw()
+{
+	if( imageLoaded === true )
+	{
+		image( userImage, 0, 0, 800, 800 );
+		modifyImage();
+	}
+}
+
+function gotFile( file )
+{
+	canvas.removeClass('dashed');
+	userImage = loadImage( file.data );
+	if(file.type === 'image')
+	{
+		imageLoaded = true;
+	}
+}
+
+function modifyImage()
+{
+	userImage.loadPixels();
+	userImage.pixels[ userImage.pixels.length / 2 ] = color( 0, 255, 0 );
+	userImage.updatePixels();
+}
