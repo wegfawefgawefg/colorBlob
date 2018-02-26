b31de183e445125079fbb976e6db88e4549296f4
vegalious <668es218pur@gmail.com>
2018-02-26 14:06:14 -0600

    asdfasdfasdfasdf

--- userDropFile/sketch.js
+++ userDropFile/sketch.js
@@ -1,10 +1,12 @@
 var canvas;
+var graphics;
 var userImage;
 var imageLoaded = false;
 
 function setup() {
 	// setup canvas
 	canvas = createCanvas( 800, 800 );
+	graphics = createGraphics( 100, 100 );
 	frameRate(1000);
 
 	//	color default look of canvas
@@ -22,7 +24,7 @@ function draw()
 {
 	if( imageLoaded === true )
 	{
-		image( userImage, 0, 0, 800, 800 );
+		image( graphics, 0, 0, 800, 800 );
 		modifyImage();
 	}
 }
@@ -31,17 +33,33 @@ function gotFile( file )
 {
 	canvas.removeClass('dashed');
 	userImage = loadImage( file.data );
+	
 	if(file.type === 'image')
 	{
 		imageLoaded = true;
 	}
 }
 
+// function modifyImage()
+// {
+// 	graphics.image( userImage, 0, 0, 100, 100 );
+//
+// 	userImage.loadPixels();
+// 	var randomX = floor( random( 100 ) );
+// 	var randomY = floor( random( 100 ) );
+// 	console.log( randomX, randomY );
+// 	userImage.set( randomX, randomY, color( 0, 255, 0 ) );
+// 	userImage.updatePixels();
+// }
+
 function modifyImage()
 {
+	graphics.image( userImage, 0, 0, 100, 100 );
+
 	userImage.loadPixels();
-	var randomPixelIndex = random( userImage.pixels.length );
-	console.log( randomPixelIndex );
-	userImage.pixels[ randomPixelIndex ] = color( 0, 255, 0 );
+	var randomX = floor( random( 100 ) );
+	var randomY = floor( random( 100 ) );
+	console.log( randomX, randomY );
+	userImage.set( randomX, randomY, color( 0, 255, 0 ) );
 	userImage.updatePixels();
 }
